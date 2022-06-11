ee21f4b5b97144bddf1d0b9bf611743853065dde
FIRST_NAME LAST_NAME <pablo_matias_g@hotmail.com>
2022-06-11 18:11:12 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componetes/home/home.component';
+import { BodyComponent } from './componetes/body/body.component';
+import { HeaderComponent } from './componetes/header/header.component';
+import { FooterComponent } from './componetes/footer/footer.component';
+import { NavComponent } from './componetes/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BodyComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
