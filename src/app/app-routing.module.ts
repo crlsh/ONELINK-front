1481274a71f882d6a11334d7f49fa87db422373f
Nicolas Gamarra <nicogamarra2010@gmail.com>
2022-06-22 13:33:27 -0300

    PaginaPrincipal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componetes/home/home.component';
+import { PhomeComponent } from './PaginaPrincipal/phome/phome.component';
 
 const routes: Routes = [
- {path: '', component:HomeComponent}
+ //{path: '', component:HomeComponent}
+ {path: '', component:PhomeComponent}
  ];
 
 @NgModule({
