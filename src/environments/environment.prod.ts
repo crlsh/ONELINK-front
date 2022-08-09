d33ee4772a21fe748cee55ae4cb74306bdaacfcc
Nicolas Gamarra <nicogamarra2010@gmail.com>
2022-08-09 18:26:36 -0300

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,28 @@
+
+import config from '../../auth_config.json';
+
+const { domain, clientId, audience, apiUri, errorPath, serverUrl } = config as {
+  domain: string;
+  clientId: string;
+  audience?: string;
+  apiUri: string;
+  errorPath: string;
+  serverUrl: string;
+};
+
 export const environment = {
-  production: true
+  production: false,
+  auth: {
+    domain,
+    clientId,
+    ...(audience && audience !== 'htpp://localhost:8080/' ? { audience } : null),
+    redirectUri: window.location.origin,
+    errorPath,
+  },
+  dev: {
+    serverUrl,
+  },
+  httpInterceptor: {
+    allowedList: [`${apiUri}/*`],
+  },
 };
